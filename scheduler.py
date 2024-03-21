dc8a4f2f18f889ee9fd87496acab2ce367849285
Joāo Chervinski <22059902+joaotav@users.noreply.github.com>
2024-03-21 15:05:51 +1100

    chore: translate code and comments into english

--- scheduler.py
+++ scheduler.py
@@ -3,201 +3,193 @@
 import argparse
 from os import system
 
-# Criação de argumentos de linha de comando
-parser = argparse.ArgumentParser(description='Simulação de escalonador.')
-# parser.add_argument('quantum', type=int, help='Janela de tempo que cada processo têm por vez para executar.')
-parser.add_argument('arquivo', type=str, help='Nome do arquivo que contem as descrições dos processos.')
+# Creation of command line arguments
+parser = argparse.ArgumentParser(description='Scheduler simulation.')
+# parser.add_argument('quantum', type=int, help='Time window that each process has in turn to execute.')
+parser.add_argument('file', type=str, help='Name of the file containing the process descriptions.')
 args = parser.parse_args()
 
-# Variáveis usadas para colorir a saída de texto
-GREEN, RED, WHITE, BLUE, YELLOW =  '\033[1;32m', '\033[91m', '\033[0m', '\33[94m', '\33[93m'
+# Variables used to color the text output
+GREEN, RED, WHITE, BLUE, YELLOW = '\033[1;32m', '\033[91m', '\033[0m', '\33[94m', '\33[93m'
 
-# Lê o arquivo de processos e coloca todos eles em uma lista
-def ler(arq):
-    processos = list()
-    with open(arq) as f:
-        for linha in f.readlines():
-            processos.append(linha.strip('\n').split())
+# Reads the process file and puts all of them into a list
+def read(file):
+    processes = list()
+    with open(file) as f:
+        for line in f.readlines():
+            processes.append(line.strip('\n').split())
 
-    for i in range(len(processos)): # Converte os números para tipos inteiros
-        processos[i][1] = int(processos[i][1])
-        processos[i][2] = int(processos[i][2])
+    for i in range(len(processes)): # Converts numbers to integer types
+        processes[i][1] = int(processes[i][1])
+        processes[i][2] = int(processes[i][2])
 
-    processos.sort(key = lambda x: x[1]) # Ordena os processo por ordem de chegada
-    return processos
+    processes.sort(key=lambda x: x[1]) # Sorts the processes by order of arrival
+    return processes
 
-def sjf(processos, lista_execucao): # Shortest Job First
-    print("{}[+]{} Utilizando Shortest Job First".format(GREEN, YELLOW))
-    # print("\n{}[+]{} Execução: \n".format(GREEN, WHITE))
-    rodada = 0
-    processos.sort(key = lambda x: (x[2], x[1])) # Ordena a fila de processos pelo tempo de duração e depois pela chegada
-    while len(processos) > 0: # Enquanto ainda houverem processos aguardando na fila
-        if processos[0][1] <= rodada: # Se o primeiro processo da fila já chegou
-            executa(processos[0], rodada, lista_execucao) # Exibe a execução
-            print(processos)
-            processos[0][2] -= 1 # Executa uma rodada do processo
-            rodada += 1
-            if processos[0][2] == 0: # Se o processo terminou a execução
-                del processos[0]
+def sjf(processes, execution_list): # Shortest Job First
+    print("{}[+]{} Using Shortest Job First".format(GREEN, YELLOW))
+    # print("\n{}[+]{} Execution: \n".format(GREEN, WHITE))
+    round_num = 0
+    processes.sort(key=lambda x: (x[2], x[1])) # Sorts the process queue by duration and then by arrival
+    while processes: # While there are still processes waiting in the queue
+        if processes[0][1] <= round_num: # If the first process in the queue has already arrived
+            execute(processes[0], round_num, execution_list) # Displays the execution
+            print(processes)
+            processes[0][2] -= 1 # Executes one round of the process
+            round_num += 1
+            if processes[0][2] == 0: # If the process has finished execution
+                del processes[0]
 
-            # Verifica se algum processo menor chegou nesse momento
-            for i in range(len(processos)):
-                # Se o processo em questão já chegou e é menor do que o atual
-                if processos[i][1] <= rodada and processos[i][2] < processos[0][2]:
-                    processos.insert(0, processos.pop(i)) # Coloca o processo no inicio da lista
+            # Checks if a shorter process arrived at this moment
+            for i in range(len(processes)):
+                # If the process in question has already arrived and is shorter than the current one
+                if processes[i][1] <= round_num and processes[i][2] < processes[0][2]:
+                    processes.insert(0, processes.pop(i)) # Moves the process to the beginning of the list
                     break
 
-        else: # Se o primeiro processo ainda não chegou
-            # Procura na fila pra ver se algum processo já chegou
-            for i in range(len(processos)):
-                if processos[i][1] <= rodada: # Se o processo em questão já chegou
-                    processos.insert(0, processos.pop(i)) # Coloca o processo no inicio da lista
+        else: # If the first process has not yet arrived
+            # Looks in the queue to see if any process has already arrived
+            for i in range(len(processes)):
+                if processes[i][1] <= round_num: # If the process in question has arrived
+                    processes.insert(0, processes.pop(i)) # Moves the process to the beginning of the list
                     break
-            else: # Se nenhum processo pode executar nesse esse momento
-                executa("wait", rodada, lista_execucao)
-                print(processos)
-                rodada += 1
+            else: # If no process can execute at this moment
+                execute("wait", round_num, execution_list)
+                print(processes)
+                round_num += 1
     return
 
-def round_robin(processos, lista_execucao):
-    print("{}[+]{} Utilizando Round-Robin".format(GREEN, YELLOW))
+
+def round_robin(processes, execution_list):
+    print(f"{GREEN}[+]{YELLOW} Using Round-Robin")
     while True:
         try:
-            quantum = int(input("{}[+]{} Digite o tamanho do quantum (janela de tempo) dedicado a cada processo por vez: ".format(GREEN, WHITE)))
+            quantum = int(input(f"{GREEN}[+]{WHITE} Enter the quantum size (time window) dedicated to each process at a time: "))
             break
-        except:
-            print("{}[-]{}Por favor, digite somente números inteiros.".format(RED, WHITE))
-    print("\n{}[+]{} Execução: \n".format(GREEN, WHITE))
-    rodada = 0
-    while len(processos) > 0: # Enquanto ainda houverem processos aguardando execução
-        if processos[0][1] <= rodada: # Se o processo no início da fila ja chegou
-            if processos[0][2] >= quantum: # Se o tamanho do quantum for menor que o nº de rodadas restantes
-                executa(processos[0], rodada, lista_execucao, quantum) # Executa o processo, quantum vezes
-                print(processos)
-                processos[0][2] -= quantum # Retira as rodadas executadas do total
-                rodada += quantum
-                if processos[0][2] == 0: # Se o processo terminou a execução
-                    del processos[0] # Retira ele da fila
+        except ValueError:
+            print(f"{RED}[-]{WHITE} Please, only enter integer numbers.")
+    print("\\n{}[+]{} Execution: \\n".format(GREEN, WHITE))
+    round_num = 0
+    while processes: # While there are still processes waiting for execution
+        if processes[0][1] <= round_num: # If the process at the start of the queue has already arrived
+            if processes[0][2] >= quantum: # If the quantum size is smaller than the number of rounds remaining
+                execute(processes[0], round_num, execution_list, quantum) # Executes the process, quantum times
+                print(processes)
+                processes[0][2] -= quantum # Subtracts the executed rounds from the total
+                round_num += quantum
+                if processes[0][2] == 0: # If the process has finished execution
+                    processes.pop(0) # Removes it from the queue
                     continue
 
-                elif len(processos) > 1:
-                    for i in range(1, len(processos)): # Vê se outro processo já chegou e pode ser executado
-                        if processos[i][1] <= rodada: # Se o processo já chegou nessa rodada
-                            aux = processos.pop(i) # Salva num nome auxiliar
-                            processos.append(processos.pop(0)) # Move o processo atual do início para o fim da fila
-                            processos.insert(0, aux) # Insere o auxiliar no início da fila
+                elif len(processes) > 1:
+                    for i in range(1, len(processes)): # Checks if another process has arrived and can be executed
+                        if processes[i][1] <= round_num: # If the process has arrived in this round
+                            aux = processes.pop(i) # Saves it under an auxiliary name
+                            processes.append(processes.pop(0)) # Moves the current process from the start to the end of the queue
+                            processes.insert(0, aux) # Inserts the auxiliary at the start of the queue
                             break
 
-                    # Se nenhum outro processo puder executar, continua executando o mesmo
+                else: # If the next process can be executed
+                    processes.append(processes.pop(0)) # Moves the current process from the start to the end of the queue
 
-                else: # Se o próximo processo puder executar
-                    processos.append(processos.pop(0)) # Move o processo atual do início para o fim da fila
-
-            else: # Se o tamanho do quantum é maior que o nº de rodadas restantes
-                executa(processos[0], rodada, lista_execucao, processos[0][2] ) # Vai executar somente o nº de rodadas restantes
-                print(processos)
-                rodada += processos[0][2] # Incrementa a rodada pelo número de rodadas executadas
-                del processos[0] # Elimina o processo da fila, pois ele terminou de executar
-        else: # Se o processo no início da fila ainda não chegou
-            executa('wait', rodada, lista_execucao)
-            print(processos)
-            rodada += 1
+            else: # If the quantum size is larger than the number of rounds remaining
+                execute(processes[0], round_num, execution_list, processes[0][2]) # Executes only the number of remaining rounds
+                print(processes)
+                round_num += processes[0][2] # Increments the round by the number of rounds executed
+                processes.pop(0) # Removes the process from the queue, as it has finished executing
+        else: # If the process at the start of the queue has not yet arrived
+            execute('wait', round_num, execution_list)
+            print(processes)
+            round_num += 1
             continue
 
-def fcfs(processos, lista_execucao):
-    print("{}[+]{} Utilizando First-come, First-served".format(GREEN, YELLOW))
-    print("\n{}[+]{} Execução: \n".format(GREEN, WHITE))
-    # Nesse caso os processos estão ordenados por ordem de chegada
-    rodada = 0
-    while len(processos) > 0: # Enquanto houverem processos na fila de execução
-        if processos[0][1] <= rodada: # Se o processo que está primeiro na fila, já chegou na rodada atual
-            executa(processos[0], rodada, lista_execucao) # Executa uma rodada do processo
-            print(processos)
-            processos[0][2] -= 1 # Reduz uma rodada das rodadas restantes para a conclusão do processo
-            rodada += 1  # Incrementa o nº de rodadas executadas
-            if processos[0][2] == 0: # Se acabou a execução do processo em questão
-                del processos[0] # Remove o processo da fila de execução
-        else: # Se o processo que está em primeiro na fila, não chegou ainda
-            executa('wait', rodada, lista_execucao)
-            print(processos)
-            rodada += 1
+def fcfs(processes, execution_list):
+    print(f"{GREEN}[+]{YELLOW} Using First-come, First-served")
+    print(f"\n{GREEN}[+]{WHITE} Execution:\n")
+    # In this case, the processes are sorted by order of arrival
+    round_num = 0
+    while processes:  # While there are processes in the execution queue
+        if processes[0][1] <= round_num:  # If the process that is first in the queue has already arrived at the current round
+            execute(processes[0], round_num, execution_list)  # Executes a round of the process
+            print(processes)
+            processes[0][2] -= 1  # Reduces a round from the remaining rounds for the completion of the process
+            round_num += 1  # Increments the number of executed rounds
+            if processes[0][2] == 0:  # If the execution of the process is finished
+                processes.pop(0)  # Removes the process from the execution queue
+        else:  # If the first process in the queue has not arrived yet
+            execute('wait', round_num, execution_list)
+            print(processes)
+            round_num += 1
 
-
-def executa(p, rodada, lista_execucao, quantum=1): # Mantém o quantum como 1 por padrão, para as estratégias diferentes de round_robin
+def execute(p, round_num, execution_list, quantum=1):  # Keeps the quantum as 1 by default, for strategies different from round_robin
     if p == 'wait':
-        lista_execucao.append(['wait'])
+        execution_list.append(['wait'])
     else:
         for i in range(quantum):
-            lista_execucao.append([p[0].upper(), p[2] - (i + 1)])
+            execution_list.append([p[0].upper(), p[2] - (i + 1)])
 
-
-def exibe(lista_execucao):
-    posicao = 1 # Começa com 1 ao invés de 0, pois range(0) não gera nenhuma iteração
+def display(execution_list):
+    position = 1  # Starts with 1 instead of 0, because range(0) does not generate any iteration
     while True:
         system('clear')
-        print("{}[+]{} Execução: \n".format(GREEN,YELLOW))
-        for i in range(posicao):
-            if lista_execucao[i][0] == 'wait':
-                print("{}[AGUARDA]".format(GREEN))
-
-            elif i > 0 and lista_execucao[i - 1][0] != lista_execucao[i][0]:
-                print ("{}[+] {}Processo {}:".format(GREEN, YELLOW, lista_execucao[i][0]))
-                print("{}[{}]{} Restantes: {}".format(GREEN, lista_execucao[i][0], WHITE, lista_execucao[i][1]))
+        print(f"{GREEN}[+]{YELLOW} Execution:\n")
+        for i in range(position):
+            if execution_list[i][0] == 'wait':
+                print(f"{GREEN}[WAIT]")
+            elif i > 0 and execution_list[i - 1][0] != execution_list[i][0]:
+                print(f"{GREEN}[+]{YELLOW} Process {execution_list[i][0]}:")
+                print(f"{GREEN}[{execution_list[i][0]}]{WHITE} Remaining: {execution_list[i][1]}")
             else:
-                print("{}[{}]{} Restantes: {}".format(GREEN, lista_execucao[i][0], WHITE, lista_execucao[i][1]))
+                print(f"{GREEN}[{execution_list[i][0]}]{WHITE} Remaining: {execution_list[i][1]}")
 
-        opt = input("\n{}[+]{} Pressione 'n' para avançar, 'b' para retroceder ou 'q' para sair: ".format(GREEN, WHITE))
+        opt = input(f"\n{GREEN}[+]{WHITE} Press 'n' to advance, 'b' to go back, or 'q' to exit: ")
 
-        if opt == 'n' and posicao < (len(lista_execucao)):
-            posicao += 1
-        elif opt == 'n' and posicao == (len(lista_execucao)):
-            print("\n{}[+]{} Concluído.\n".format(GREEN, WHITE))
+        if opt == 'n' and position < len(execution_list):
+            position += 1
+        elif opt == 'n' and position == len(execution_list):
+            print(f"\n{GREEN}[+]{WHITE} Completed.\n")
             break
-        elif opt == 'b' and posicao > 0:
-            posicao -= 1
-
+        elif opt == 'b' and position > 1:
+            position -= 1
         elif opt == 'q':
             raise SystemExit
 
-
-def cabecalho(processos, lista_execucao):
-    modos = [fcfs, sjf, round_robin]
+def header(processes, execution_list):
+    modes = [fcfs, sjf, round_robin]
     while True:
-        opt = input("\n{}-> Escolha o algoritmo de escalonamento a ser executado:\n\n".format(GREEN)
+        opt = input("\n{}-> Choose the scheduling algorithm to be executed:\n\n".format(GREEN)
         + "{}[0]{} First-come, First-served\n".format(BLUE, WHITE)
         + "{}[1]{} Shortest Job First\n".format(BLUE, WHITE)
         + "{}[2]{} Round-Robin\n".format(BLUE, WHITE)
-        + "{}[3]{} Sair do programa\n".format(BLUE, WHITE)
-        + "\n{}[+]{} Sua opção: ".format(GREEN, WHITE))
+        + "{}[3]{} Exit the program\n".format(BLUE, WHITE)
+        + "\n{}[+]{} Your option: ".format(GREEN, WHITE))
 
-        if opt in ['0','1','2']:
-            modos[int(opt)](processos, lista_execucao)
-            exibe(lista_execucao)
+        if opt in ['0', '1', '2']:
+            modes[int(opt)](processes, execution_list)
+            display(execution_list)
         elif opt == '3':
             return opt
             break
         else:
-            print("\n{}[-]{} Por favor, escolha uma opção válida.\n".format(RED, WHITE))
+            print("\n{}[-]{} Please choose a valid option.\n".format(RED, WHITE))
 
-
-def main(arq, out=True):
-    GREEN, RED, WHITE, BLUE =  '\033[1;32m', '\033[91m', '\033[0m', '\033[94m'
-    processos = ler(arq)
-    lista_execucao = list()
-    opt = cabecalho(processos, lista_execucao)
+def main(file, out=True):
+    GREEN, RED, WHITE, BLUE = '\\033[1;32m', '\\033[91m', '\\033[0m', '\\033[94m'
+    processes = read(file)
+    execution_list = list()
+    opt = header(processes, execution_list)
     if opt == '3':
-        print("\n{}[-]{} Encerrando...".format(RED, WHITE))
+        print("\n{}[-]{} Shutting down...".format(RED, WHITE))
     else:
-        exibe(lista_execucao)
-        main(args.arquivo)
+        display(execution_list)
+        main(args.file)
 
-
-print ("""{}
+print("""{}
   ______               _                       _
  |  ____|             | |                     | |
  | |__   ___  ___ __ _| | ___  _ __   __ _  __| | ___  _ __
- |  __| / __|/ __/ _` | |/ _ \| '_ \ / _` |/ _` |/ _ \| '__|
- | |____\__ \ (_| (_| | | (_) | | | | (_| | (_| | (_) | |
- |______|___/\___\__,_|_|\___/|_| |_|\__,_|\__,_|\___/|_|
-     """.format('\033[1;32m'))
-main(args.arquivo)
+ |  __| / __|/ __/ _` | |/ _ \| '_ \\ / _` |/ _` |/ _ \\| '__|
+ | |____\\__ \\ (_| (_| | | (_) | | | | (_| | (_| | (_) | |
+ |______|___/\\___\\__,_|_|\\___/|_| |_|\\__,_|\\__,_|\\___/|_|
+     """.format(GREEN))
+main(args.file)
